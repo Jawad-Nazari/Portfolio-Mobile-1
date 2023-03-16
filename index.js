4358df3b22f2090e4f54709db1eedf42d91f89b0
Jawad <Jawad.Nazari@outlook.com>
2023-03-16 20:36:26 +0500

    update the image size

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ for (let i = 0; i < menuItem.length; i += 1) {
 const data = [
   {
     projectTitle: 'Tonic',
-    imageURL: './image/card1.svg',
+    imageURL: './image/card2.svg',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
     technologies: ['HTML', 'CSS', 'JS', 'Ruby'],
     seeLive: 'https://jawad-nazari.github.io/',
