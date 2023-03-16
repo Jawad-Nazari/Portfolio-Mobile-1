3d8a779d2c64f65575d2ee4d6fdb9b96804b43c0
Emmanuel <emmanuelnwoye12@gmail.com>
2023-03-16 19:43:14 +0100

    Update js file

--- index.js
+++ index.js
@@ -120,3 +120,12 @@ document.querySelectorAll('.btn').forEach((occurence) => {
 const formContainer = document.querySelector('.con-form');
 const mailInput = document.getElementById('mail');
 const messageError = document.querySelector('.error-message');
+
+formContainer.addEventListener('submit', (e) => {
+  if (mailInput.value === mailInput.value.toLowerCase()) {
+    messageError.textContent = '';
+  } else {
+    e.preventDefault();
+    messageError.innerHTML = '*email must be in lower case <br> * form not sent';
+  }
+});
