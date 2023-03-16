30d4e8767c28023b988fa945b4d8983c61d3f3e1
Emmanuel <emmanuelnwoye12@gmail.com>
2023-03-16 06:15:35 +0100

    Update js file

--- index.js
+++ index.js
@@ -58,3 +58,63 @@ const data = [
     sourceCode: 'https://github.com/Jawad-Nazari/Portfolio-Mobile-1',
   },
 ];
+
+document.querySelectorAll('.btn').forEach((occurence) => {
+  const id = occurence.getAttribute('id');
+  const popupCard = `
+  <div class="project1" id="project1">
+        <div class="popupTop">
+            <div class="projectPopupTitle">
+              <div class="titlePopup">
+                <h3 class="comp-title">${data[id].projectTitle}</h3>
+                <img
+                id="popupClose${id}"
+                class="popupClose"
+                src="./icons/close.svg"
+                alt="closeBtn"
+                />
+              </div>
+              <ul class="work-title">
+                    <li class="client">CANOPY</li>
+                    <li><img src="./image/Counter.svg" alt="dot" /></li>
+                    <li class="dim">Back End Dev</li>
+                    <li><img src="./image/Counter.svg" alt="dot" /></li>
+                    <li class="dim">2015</li>
+                </ul>
+            </div>
+            <img class="projectImage" src=${data[id].imageURL} alt="projectImage">
+          <div class="popupDown">
+          <div class="project-desc popupDesc">${data[id].description}</div>
+            <div class="popupBtns">
+              <ul class="tags">
+                <li class="tag">${data[id].technologies[0]}</li>
+                <li class="tag">${data[id].technologies[1]}</li>
+                <li class="tag">${data[id].technologies[2]}</li>
+                <li class="tag">${data[id].technologies[3]}</li>
+              </ul>
+              <hr>
+              <div class="actionBtnPopup">
+               <a class="btn btnPopup" href=${data[id].seeLive} target='blank'>
+               See Live <img class="seeAll" src='./Icons/Icon.svg' alt="seeLive"></a>
+              <a class="btn btnPopup" href=${data[id].sourceCode} target='blank'>
+              See Source <img class="seeAll" src='./icons/github.svg' alt="githubCode"></a>
+              </div>
+            </div>
+          </div>
+          </div>
+      </div>
+    </div>`;
+  occurence.addEventListener('click', () => {
+    const body = document.getElementById('body');
+    const section = document.createElement('section');
+    section.innerHTML = popupCard;
+    section.classList.add('hide');
+    body.appendChild(section);
+    section.classList.add('projectPopup');
+    if (section.classList.contains('hide')) { section.classList.remove('hide'); }
+    const hidePopup = document.getElementById(`popupClose${id}`);
+    hidePopup.addEventListener('click', () => {
+      body.removeChild(section);
+    });
+  });
+});
